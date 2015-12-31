db7d0f998a5aa7074930535807c19d7f929708b6
saihou <sai-hou@hotmail.com>
2015-12-31 15:34:28 -0800

    Created a disc prefab

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject disc;
+
+	// Use this for initialization
+	void Start () {
+		GameObject d = GameObject.Instantiate(disc);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/SpawnPositions.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPositions : MonoBehaviour {
+
+	public static SpawnPositions instance;
+	Vector3 left = new Vector3 (-8.0f, 10.0f, 0.0f);
+	Vector3 mid = new Vector3 (-5.0f, 10.0f, 0.0f);
+	Vector3 right = new Vector3 (-2.0f, 10.0f, 0.0f);
+
+	void Awake() {
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	Vector3 GetPos(DiscSpawnPoints dsp) {
+		switch (dsp) {
+			case DiscSpawnPoints.LEFT: return left;
+			case DiscSpawnPoints.MID: return mid;
+			case DiscSpawnPoints.RIGHT: return right;
+		}
+		return Vector3.zero;
+	}
+}
+
+enum DiscSpawnPoints { LEFT, MID, RIGHT };
