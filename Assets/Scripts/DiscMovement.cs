eec19c001d6591ac2eef56336eb5e56a54a42993
saihou <sai-hou@hotmail.com>
2015-12-31 15:00:34 -0800

    Added new Unity Project

---
+++ Assets/Scripts/DiscMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiscMovement : MonoBehaviour {
+
+	public float speed = 0.05f;
+	float defaultSpeed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = gameObject.transform.position + speed*Vector3.down;
+	}
+
+	public void SetSpeed(float newSpeed) {
+		speed = newSpeed;
+	}
+
+	public void ResetSpeed() {
+		speed = defaultSpeed;
+	}
+}
